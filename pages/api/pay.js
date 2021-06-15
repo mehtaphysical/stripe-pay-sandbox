5c1c89e023b75b6643314d4d820a1ce1fa6315ce
Ryan Mehta <ryan.mehta@gmail.com>
2021-06-15 12:55:25 -0700

    add cors

--- pages/api/pay.js
+++ pages/api/pay.js
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import Stripe from "stripe";
 import { mintTokens } from "../../services/near";
 
@@ -6,31 +7,32 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 });
 
 export default async (req, res) => {
-  const { accountId, paymentMethodId, amount } = req.body;
-  console.log(amount);
+  cors()(req, res, () => {
+    const { accountId, paymentMethodId, amount } = req.body;
 
-  try {
-    const intent = await stripe.paymentIntents.create({
-      amount,
-      currency: "usd",
-      payment_method_types: ["card"],
-      capture_method: "manual",
-      payment_method: paymentMethodId,
-      confirm: true,
-      return_url: `${process.env.HOST_NAME}/api/${accountId}/complete`,
-    });
-
-    let outcome = null;
-    if (intent.status === "requires_capture" && !intent.next_action) {
-      outcome = await mintTokens({
-        accountId,
+    try {
+      const intent = await stripe.paymentIntents.create({
         amount,
-        intentId: intent.id,
+        currency: "usd",
+        payment_method_types: ["card"],
+        capture_method: "manual",
+        payment_method: paymentMethodId,
+        confirm: true,
+        return_url: `${process.env.HOST_NAME}/api/${accountId}/complete`,
       });
-    }
 
-    res.status(200).json({ intent, outcome });
-  } catch (err) {
-    res.status(400).json(err);
-  }
+      let outcome = null;
+      if (intent.status === "requires_capture" && !intent.next_action) {
+        outcome = await mintTokens({
+          accountId,
+          amount,
+          intentId: intent.id,
+        });
+      }
+
+      res.status(200).json({ intent, outcome });
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  })
 };
